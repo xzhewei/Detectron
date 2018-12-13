3babb64f7f10427ee29449ed33018a048acc76e7
xuzhewei <xzhewei@gmail.com>
2018-12-13 08:58:52 +0800

    exp:scut
    MR-2
    Reasonable 0.073118
    All 0.234057
    Scale=near 0.018701
    Scale=medium 0.172438
    Scale=far 0.442968
    Occ=none 0.047820
    Occ=partial 0.165665
    
    MR-4
    Reasonable 0.178177
    All 0.389731
    Scale=near 0.079618
    Scale=medium 0.324822
    Scale=far 0.619209
    Occ=none 0.133149
    Occ=partial 0.365178

--- test/train_net.sh
+++ test/train_net.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 dataset=scut
-GPU=S0_G2
-method_name=e2e_frcnn_VGG16_C5d2_im1.5
+GPU=S0_G3
+method_name=e2e_frcnn_VGG16-C5d2-im1.5
 
 MAX_ITER=180000
 
@@ -11,4 +11,5 @@ train_dir=${output_dir}/train/scut_train_10x/generalized_rcnn
 python tools/train_net.py \
     --cfg configs/${dataset}/${method_name}.yaml \
     OUTPUT_DIR $output_dir \
-    SOLVER.MAX_ITER $MAX_ITER
+    SOLVER.MAX_ITER $MAX_ITER \
+    EXP_ID 2018121D01
