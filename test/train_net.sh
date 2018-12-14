9a3c11f8aad09d5864e423d5b3092a43632981ee
xuzhewei <xzhewei@gmail.com>
2018-12-14 22:22:55 +0800

    multi-gpu training is better
    MR-2
    Reasonable 0.116617
    All 0.318621
    Scale=near 0.037638
    Scale=medium 0.257083
    Scale=far 0.545464
    Occ=none 0.085455
    Occ=partial 0.237214
    
    MR-4
    Reasonable 0.253544
    All 0.486967
    Scale=near 0.122293
    Scale=medium 0.423740
    Scale=far 0.716091
    Occ=none 0.206592
    Occ=partial 0.447659

--- test/train_net.sh
+++ test/train_net.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 dataset=scut
-GPU=S0_G3
-method_name=e2e_frcnn_VGG16-C5d2-im1.5
+GPU=S1_G0-3
+method_name=e2e_frcnn_VGG16-C5
 
-MAX_ITER=180000
+NUM_GPUS=4
 
 output_dir=output/${dataset}/${GPU}-${method_name}/detectron-output
 train_dir=${output_dir}/train/scut_train_10x/generalized_rcnn
@@ -11,5 +11,5 @@ train_dir=${output_dir}/train/scut_train_10x/generalized_rcnn
 python tools/train_net.py \
     --cfg configs/${dataset}/${method_name}.yaml \
     OUTPUT_DIR $output_dir \
-    SOLVER.MAX_ITER $MAX_ITER \
-    EXP_ID 2018121D01
+    NUM_GPUS $NUM_GPUS \
+    EXP_ID 20181214D01
