1550856ba729f468c738efed5d2115ed5c32f322
xuzhewei <xzhewei@gmail.com>
2019-01-02 11:07:39 +0800

    exp:S1:4GPU:S1_e2e_frcnn_VGG16-C5_roadline
    MR-2
    Reasonable 0.108158
    All 0.312106
    Scale=near 0.027593
    Scale=medium 0.248352
    Scale=far 0.558300
    Occ=none 0.077221
    Occ=partial 0.223793
    
    MR-4
    Reasonable 0.244055
    All 0.479632
    Scale=near 0.106714
    Scale=medium 0.416826
    Scale=far 0.723669
    Occ=none 0.199907
    Occ=partial 0.432731

--- test/train_net_lab1.sh
+++ test/train_net_lab1.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 dataset=scut
 GPU=S1_G0-3
-method_name=e2e_frcnn_VGG16-C5d2-im1.5
+method_name=S1_e2e_frcnn_VGG16-C5_roadline
 
 NUM_GPUS=4
 
@@ -10,6 +10,6 @@ train_dir=${output_dir}/train/scut_train_10x/generalized_rcnn
 
 python tools/train_net.py \
     --cfg configs/${dataset}/${method_name}.yaml \
-    OUTPUT_DIR $output_dir \
+    OUTPUT_DIR ${output_dir}_bak \
     NUM_GPUS $NUM_GPUS \
-    EXP_ID 20181214D02
+    EXP_ID 20181220D01
