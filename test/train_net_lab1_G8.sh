b312f6f98954cfdbdf3088fb603cb698c7f5f6e6
xuzhewei <xzhewei@gmail.com>
2019-01-09 10:01:56 +0800

    exp:caltech:S1:8GPU:S1_G0-7-e2e_frcnn_VGG16-C5
    1.5x image size
    
    MR-2
    Reasonable 0.197463
    All 0.596494
    Scale=large 0.023468
    Scale=near 0.048841
    Scale=medium 0.472802
    Scale=far 0.862376
    Occ=none 0.175146
    Occ=partial 0.381209
    Occ=heavy 0.652028
    Ar=all 0.175146
    Ar=typical 0.162263
    Ar=atypical 0.235399
    Overlap=25 0.160371
    Overlap=50 0.197463
    Overlap=75 0.502039
    Expand=100 0.210488
    Expand=125 0.197463
    Expand=150 0.207449
    
    MR-4
    Reasonable 0.347307
    All 0.720157
    Scale=large 0.077587
    Scale=near 0.134021
    Scale=medium 0.626382
    Scale=far 0.916324
    Occ=none 0.320677
    Occ=partial 0.554536
    Occ=heavy 0.774103
    Ar=all 0.320677
    Ar=typical 0.302541
    Ar=atypical 0.407974
    Overlap=25 0.303401
    Overlap=50 0.347307
    Overlap=75 0.683057
    Expand=100 0.355888
    Expand=125 0.347307
    Expand=150 0.358296

--- test/train_net_lab1_G8.sh
+++ test/train_net_lab1_G8.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-dataset=scut
+dataset=caltech
 GPU=S1_G0-7
 method_name=S1_e2e_frcnn_VGG16-C5
 
@@ -12,4 +12,4 @@ python tools/train_net.py \
     --cfg configs/${dataset}/${method_name}.yaml \
     OUTPUT_DIR $output_dir \
     NUM_GPUS $NUM_GPUS \
-    EXP_ID 20190108D01
+    EXP_ID 20190108D02
