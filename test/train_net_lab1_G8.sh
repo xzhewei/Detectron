2a47cfb26548044adc640c6b995fa2af61cd01a1
xuzhewei <xzhewei@gmail.com>
2019-01-09 22:34:30 +0800

    exp:caltech:S1:8GPU:S1_G0-7-e2e_faster_rcnn_VGG-16-C5_10x
    MR-2
    Reasonable 0.175666
    All 0.584687
    Scale=large 0.030269
    Scale=near 0.055014
    Scale=medium 0.462271
    Scale=far 0.814961
    Occ=none 0.156170
    Occ=partial 0.298733
    Occ=heavy 0.613408
    Ar=all 0.156170
    Ar=typical 0.131664
    Ar=atypical 0.276720
    Overlap=25 0.126147
    Overlap=50 0.175666
    Overlap=75 0.618248
    Expand=100 0.203128
    Expand=125 0.175666
    Expand=150 0.186230
    
    MR-4
    Reasonable 0.304040
    All 0.697692
    Scale=large 0.080409
    Scale=near 0.128644
    Scale=medium 0.600517
    Scale=far 0.890943
    Occ=none 0.277894
    Occ=partial 0.481262
    Occ=heavy 0.758645
    Ar=all 0.277894
    Ar=typical 0.245528
    Ar=atypical 0.425535
    Overlap=25 0.250918
    Overlap=50 0.304040
    Overlap=75 0.768349
    Expand=100 0.329082
    Expand=125 0.304040
    Expand=150 0.313587

--- test/train_net_lab1_G8.sh
+++ test/train_net_lab1_G8.sh
@@ -6,10 +6,10 @@ method_name=e2e_faster_rcnn_VGG-16-C5_10x
 NUM_GPUS=8
 
 output_dir=output/${dataset}/${GPU}-${method_name}/detectron-output
-train_dir=${output_dir}/train/caltech_train_10x/generalized_rcnn
+#train_dir=${output_dir}/train/caltech_train_10x/generalized_rcnn
 
 python tools/train_net.py \
     --cfg configs/${dataset}/${method_name}.yaml \
     OUTPUT_DIR $output_dir \
     NUM_GPUS $NUM_GPUS \
-    EXP_ID 20190109D01
+    EXP_ID 20190109D02
